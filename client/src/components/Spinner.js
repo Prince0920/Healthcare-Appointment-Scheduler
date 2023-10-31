d4bb0086cf426646d46fcab4ff4c01868a2f465e
Prince Agrawal <prince.agrawal@dotsquares.com>
2023-10-31 16:33:33 +0530

    lodder placement correction

--- client/src/components/Spinner.js
+++ client/src/components/Spinner.js
@@ -1,31 +1,26 @@
-import React from "react"
+import React from 'react';
 
-import { useState, CSSProperties } from "react";
-import MoonLoader from "react-spinners/ClipLoader";
-const override: CSSProperties = {
-    display: "block",
-    margin: "10 10 auto",
-    borderColor: "#7096bf",
-  };
+import { useState, CSSProperties } from 'react';
+import MoonLoader from 'react-spinners/ClipLoader';
+const override = {
+  display: 'block',
+  margin: '10 10 auto',
+  borderColor: '#7096bf',
+};
 
-const Spinner = ()=>{
- 
+const Spinner = () => {
+  let [color, setColor] = useState('#ffffff');
 
-    let [color, setColor] = useState("#ffffff");
-
-
-    return(
- 
-        <MoonLoader
-        color={color}
-        size={150}
-        cssOverride={override}
-        aria-label="Loading Spinner"
-        data-testid="loader"
-      />
-
-    )
-
-
-}
+  return (
+    <div className='row justify-content-center'>
+      <MoonLoader
+      color={color}
+      size={150}
+      cssOverride={override}
+      aria-label='Loading Spinner'
+      data-testid='loader'
+    />
+    </div>
+  );
+};
 export default Spinner;
