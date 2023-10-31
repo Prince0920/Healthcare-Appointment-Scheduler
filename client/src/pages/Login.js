33a66fa6e12f8f1743982a8e5a8d9671367d8cb2
Prince Agrawal <prince.agrawal@dotsquares.com>
2023-10-31 16:12:16 +0530

    login register form correction

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,33 +1,33 @@
-import axios from "axios";
-import React, { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import { ToastContainer, toast } from "react-toastify";
-import { useDispatch } from "react-redux";
-import { showLoading,hideLoading } from "../redux/features/alertSlice";
+import axios from 'axios';
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { ToastContainer, toast } from 'react-toastify';
+import { useDispatch } from 'react-redux';
+import { showLoading, hideLoading } from '../redux/features/alertSlice';
 import { SERVER_BASE_URL } from '../config/config';
 
 const Login = () => {
   const navigate = useNavigate();
 
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [errors, setErrors] = useState("");
-  const dispatch  = useDispatch();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [errors, setErrors] = useState('');
+  const dispatch = useDispatch();
 
   var validateLoginForm = (email, password) => {
     let errors = {};
     if (!email) {
-      errors.email = "*E-mail is required";
+      errors.email = '*E-mail is required';
     }
     if (!password) {
-      errors.password = "*Password is required";
+      errors.password = '*Password is required';
     }
     setErrors(errors);
     return Object.keys(errors).length === 0;
   };
 
-  const handelLoginForm = async (e) => {
-    console.log("SERVER_URLSERVER_URLSERVER_URLSERVER_URLSERVER_URLSERVER_URL",SERVER_BASE_URL)
+  const handelLoginForm = async e => {
+    console.log('SERVER_URLSERVER_URLSERVER_URLSERVER_URLSERVER_URLSERVER_URL', SERVER_BASE_URL);
     e.preventDefault();
     const isValidate = validateLoginForm(email, password);
 
@@ -38,97 +38,97 @@ const Login = () => {
       };
 
       try {
-          dispatch(showLoading())
-        const res = await axios.post(SERVER_BASE_URL+
-          "/api/v1/user/login",
+        dispatch(showLoading());
+        const res = await axios.post(
+          SERVER_BASE_URL + '/api/v1/user/login',
           JSON.stringify(loginData),
           {
-            headers: { "content-type": "application/json" },
+            headers: { 'content-type': 'application/json' },
           }
         );
 
         if (res.data.success) {
-          dispatch(hideLoading())
+          dispatch(hideLoading());
           toast.success(res.data.message);
-          localStorage.setItem("token", res.data.token);
-          navigate("/");
+          localStorage.setItem('token', res.data.token);
+          navigate('/');
         }
         if (!res.data.success) {
-          dispatch(hideLoading())
+          dispatch(hideLoading());
           toast.error(res.data.message);
         }
       } catch (error) {
-        dispatch(hideLoading())
+        dispatch(hideLoading());
         console.log(error);
       }
     }
   };
 
   return (
-    <div className="login-box">
-      <div className="login-logo">
-        <a href="#">
-          <img
-            src="asserts/logo/health.png"
-            alt="AdminLTE Logo"
-            className="brand-image img-circle elevation-3"
-            style={{ opacity: "1" }}
-            height="150"
-            width="150"
-          />
-          {/* <b>Admin</b>LTE */}
-        </a>
-      </div>
-      <div className="card">
-        <div className="card-body login-card-body">
-          <p className="login-box-msg">Sign in to start your session</p>
-          <form method="post" onSubmit={handelLoginForm}>
-            <span className="error-text">
-              {errors.email ? errors.email : ""}
-            </span>
-            <div className="input-group mb-3">
-              <input
-                type="email"
-                className="form-control"
-                name="email"
-                value={email}
-                placeholder="Email"
-                onChange={(e) => setEmail(e.target.value)}
+    <div className='container login-box mt-3'>
+      <div className='row justify-content-center card'>
+        <div className='card-body login-card-body'>
+          <div className='login-logo'>
+            <a href='#'>
+              <img
+                src='asserts/logo/health.png'
+                alt='AdminLTE Logo'
+                className='brand-image img-circle elevation-3'
+                style={{ opacity: '1' }}
+                height='150'
+                width='150'
               />
-              <div className="input-group-append">
-                <div className="input-group-text">
-                  <span className="fas fa-envelope" />
+              {/* <b>Admin</b>LTE */}
+            </a>
+          </div>
+          <p className='login-box-msg'>Sign in to start your session</p>
+          <form
+            method='post'
+            onSubmit={handelLoginForm}>
+            <span className='error-text'>{errors.email ? errors.email : ''}</span>
+            <div className='input-group mb-3'>
+              <input
+                type='email'
+                className='form-control'
+                name='email'
+                value={email}
+                placeholder='Email'
+                onChange={e => setEmail(e.target.value)}
+              />
+              <div className='input-group-append'>
+                <div className='input-group-text'>
+                  <span className='fas fa-envelope' />
                 </div>
               </div>
             </div>
 
-            <span className="error-text">
-              {errors.password ? errors.password : ""}
-            </span>
-            <div className="input-group mb-3">
+            <span className='error-text'>{errors.password ? errors.password : ''}</span>
+            <div className='input-group mb-3'>
               <input
-                type="password"
-                className="form-control"
-                name="password"
+                type='password'
+                className='form-control'
+                name='password'
                 value={password}
-                placeholder="Password"
-                onChange={(e) => setPassword(e.target.value)}
+                placeholder='Password'
+                onChange={e => setPassword(e.target.value)}
               />
-              <div className="input-group-append">
-                <div className="input-group-text">
-                  <span className="fas fa-lock" />
+              <div className='input-group-append'>
+                <div className='input-group-text'>
+                  <span className='fas fa-lock' />
                 </div>
               </div>
             </div>
-            <div className="row">
-              <div className="col-8">
+            <div className='row'>
+              <div className='col-8'>
                 {/* <div className="icheck-primary">
                   <input type="checkbox" id="remember" />
                   <label htmlFor="remember">Remember Me</label>
                 </div> */}
               </div>
-              <div className="col-4">
-                <button type="submit" className="btn btn-primary btn-block">
+              <div className='col-4'>
+                <button
+                  type='submit'
+                  className='btn btn-primary btn-block'>
                   Sign In
                 </button>
               </div>
@@ -146,13 +146,16 @@ const Login = () => {
           {/* <p className="mb-1">
             <a href="forgot-password.html">I forgot my password</a>
           </p> */}
-          <p className="mb-0">
-            <Link to="/register" className="text-center">
+          <p className='mb-0'>
+            <Link
+              to='/register'
+              className='text-center'>
               Register
             </Link>
           </p>
         </div>
       </div>
+
       <ToastContainer />
     </div>
   );
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,53 +1,46 @@
-import React, { useState } from "react";
-import { ToastContainer, toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-import { Link } from "react-router-dom";
-import axios from "axios";
-import { useDispatch } from "react-redux";
-import { showLoading,hideLoading } from "../redux/features/alertSlice";
-import { SERVER_BASE_URL } from "../config/config.local";
+import React, { useState } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+import { useDispatch } from 'react-redux';
+import { showLoading, hideLoading } from '../redux/features/alertSlice';
+import { SERVER_BASE_URL } from '../config/config.local';
 
 const Register = () => {
-  const [fullname, setFullname] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [usertype, setUsertype] = useState("");
-  const [confirm_password, setConfirm_password] = useState("");
+  const [fullname, setFullname] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [usertype, setUsertype] = useState('');
+  const [confirm_password, setConfirm_password] = useState('');
   const [agreeTerms, setAgreeTerms] = useState(false);
 
   const [errors, setErrors] = useState({});
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
-  const validateForm = (
-    fullname,
-    email,
-    password,
-    confirm_password,
-    usertype,
-    agreeTerms
-  ) => {
+  const validateForm = (fullname, email, password, confirm_password, usertype, agreeTerms) => {
     const errors = {};
     if (!fullname) {
-      errors.fullname = "*Full name is required";
+      errors.fullname = '*Full name is required';
     }
     if (!email) {
-      errors.email = "*E-mail is required";
+      errors.email = '*E-mail is required';
     }
     if (!password) {
-      errors.password = "*Password is required";
+      errors.password = '*Password is required';
     }
     if (!confirm_password) {
-      errors.confirm_password = "*Confirm password is required";
+      errors.confirm_password = '*Confirm password is required';
     }
     if (!usertype) {
-      errors.usertype = "*Registration type is required";
+      errors.usertype = '*Registration type is required';
     }
     if (password != confirm_password) {
-      errors.confirm_password = "*Confirm password does not match";
+      errors.confirm_password = '*Confirm password does not match';
     }
 
     if (!agreeTerms) {
-      errors.agreeTerms = "*Please check terms and condition";
+      errors.agreeTerms = '*Please check terms and condition';
     }
 
     setErrors(errors);
@@ -58,17 +51,10 @@ const Register = () => {
     setAgreeTerms(!agreeTerms);
   };
 
-  const handelRegForm = async (e) => {
+  const handelRegForm = async e => {
     e.preventDefault();
 
-    const isValid = validateForm(
-      fullname,
-      email,
-      password,
-      confirm_password,
-      usertype,
-      agreeTerms
-    );
+    const isValid = validateForm(fullname, email, password, confirm_password, usertype, agreeTerms);
 
     if (isValid) {
       const registerData = {
@@ -78,27 +64,27 @@ const Register = () => {
         password: password,
       };
       try {
-        dispatch(showLoading())
-        const res = await axios.post(SERVER_BASE_URL+
-          "api/v1/user/register",
+        dispatch(showLoading());
+        const res = await axios.post(
+          SERVER_BASE_URL + 'api/v1/user/register',
           JSON.stringify(registerData),
-          { headers: { "content-type": "application/json" } }
+          { headers: { 'content-type': 'application/json' } }
         );
 
         if (res.data.success) {
-          dispatch(hideLoading())
+          dispatch(hideLoading());
           toast.success(res.data.message);
-          setFullname("");
-          setEmail("");
-          setPassword("");
-          setUsertype("");
-          setConfirm_password("");
-          setAgreeTerms("");
+          setFullname('');
+          setEmail('');
+          setPassword('');
+          setUsertype('');
+          setConfirm_password('');
+          setAgreeTerms('');
         }
         //alert(res.data.message);
         if (!res.data.success) {
-          dispatch(hideLoading())
-          toast.error(res.data.message ? res.data.message : "");
+          dispatch(hideLoading());
+          toast.error(res.data.message ? res.data.message : '');
         }
       } catch (error) {
         toast.error(error);
@@ -108,135 +94,134 @@ const Register = () => {
   };
 
   return (
-    <div className="register-box">
-      <div className="register-logo">
-        <a href="#">
-          <img
-            src="asserts/logo/health.png"
-            alt="AdminLTE Logo"
-            className="brand-image img-circle elevation-3"
-            style={{ opacity: "1" }}
-            height="150"
-            width="150"
-          />
-          {/* <b>Admin</b>LTE */}
-        </a>
-      </div>
-      <div className="card">
-        <div className="card-body register-card-body">
-          <p className="login-box-msg">Register here</p>
+    <div className='container register-box mt-3'>
+      <div className='card row justify-content-center'>
+        <div className='card-body register-card-body'>
+          <div className='register-logo'>
+            <a href='#'>
+              <img
+                src='asserts/logo/health.png'
+                alt='AdminLTE Logo'
+                className='brand-image img-circle elevation-3'
+                style={{ opacity: '1' }}
+                height='150'
+                width='150'
+              />
+              {/* <b>Admin</b>LTE */}
+            </a>
+          </div>
+          <p className='login-box-msg'>Register here</p>
           <form onSubmit={handelRegForm}>
-            <span className="error-text">{errors.usertype}</span>
-            <div className="input-group mb-3">
+            <span className='error-text'>{errors.usertype}</span>
+            <div className='input-group mb-3'>
               <select
-                className="form-control"
-                name="usertype"
+                className='form-control'
+                name='usertype'
                 value={usertype}
-                onChange={(e) => setUsertype(e.target.value)}
-              >
-                <option value="">--Select Type--</option>
-                <option value="patient">Patient</option>
-                <option value="doctor">Doctor</option>
-                <option value="clinic">Clinic</option>
-                <option value="hospital">Hospital</option>
+                onChange={e => setUsertype(e.target.value)}>
+                <option value=''>--Select Type--</option>
+                <option value='patient'>Patient</option>
+                <option value='doctor'>Doctor</option>
+                <option value='clinic'>Clinic</option>
+                <option value='hospital'>Hospital</option>
               </select>
 
-              <div className="input-group-append">
-                <div className="input-group-text">
-                  <span className="fas fa-briefcase-medical" />
+              <div className='input-group-append'>
+                <div className='input-group-text'>
+                  <span className='fas fa-briefcase-medical' />
                 </div>
               </div>
             </div>
 
-            <span className="error-text">{errors.fullname}</span>
-            <div className="input-group mb-3">
+            <span className='error-text'>{errors.fullname}</span>
+            <div className='input-group mb-3'>
               <input
-                type="text"
-                className="form-control"
-                name="fullname"
+                type='text'
+                className='form-control'
+                name='fullname'
                 value={fullname}
-                placeholder="Full name"
-                onChange={(e) => setFullname(e.target.value)}
+                placeholder='Full name'
+                onChange={e => setFullname(e.target.value)}
               />
-              <div className="input-group-append">
-                <div className="input-group-text">
-                  <span className="fas fa-user" />
+              <div className='input-group-append'>
+                <div className='input-group-text'>
+                  <span className='fas fa-user' />
                 </div>
               </div>
             </div>
 
-            <span className="error-text">
-              {errors.email ? errors.email : ""}
-            </span>
-            <div className="input-group mb-3">
+            <span className='error-text'>{errors.email ? errors.email : ''}</span>
+            <div className='input-group mb-3'>
               <input
-                type="email"
-                className="form-control"
-                name="email"
+                type='email'
+                className='form-control'
+                name='email'
                 value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                placeholder="Email"
+                onChange={e => setEmail(e.target.value)}
+                placeholder='Email'
               />
-              <div className="input-group-append">
-                <div className="input-group-text">
-                  <span className="fas fa-envelope" />
+              <div className='input-group-append'>
+                <div className='input-group-text'>
+                  <span className='fas fa-envelope' />
                 </div>
               </div>
             </div>
 
-            <span className="error-text">{errors.password}</span>
-            <div className="input-group mb-3">
+            <span className='error-text'>{errors.password}</span>
+            <div className='input-group mb-3'>
               <input
-                type="password"
-                className="form-control"
-                name="password"
+                type='password'
+                className='form-control'
+                name='password'
                 value={password}
-                placeholder="Password"
-                onChange={(e) => setPassword(e.target.value)}
+                placeholder='Password'
+                onChange={e => setPassword(e.target.value)}
               />
-              <div className="input-group-append">
-                <div className="input-group-text">
-                  <span className="fas fa-lock" />
+              <div className='input-group-append'>
+                <div className='input-group-text'>
+                  <span className='fas fa-lock' />
                 </div>
               </div>
             </div>
 
-            <span className="error-text">{errors.confirm_password}</span>
-            <div className="input-group mb-3">
+            <span className='error-text'>{errors.confirm_password}</span>
+            <div className='input-group mb-3'>
               <input
-                type="password"
-                className="form-control"
-                name="confirm_password"
+                type='password'
+                className='form-control'
+                name='confirm_password'
                 value={confirm_password}
-                placeholder="Retype password"
-                onChange={(e) => setConfirm_password(e.target.value)}
+                placeholder='Retype password'
+                onChange={e => setConfirm_password(e.target.value)}
               />
-              <div className="input-group-append">
-                <div className="input-group-text">
-                  <span className="fas fa-lock" />
+              <div className='input-group-append'>
+                <div className='input-group-text'>
+                  <span className='fas fa-lock' />
                 </div>
               </div>
             </div>
 
-            <div className="row">
-              <span className="error-text">{errors.agreeTerms}</span>
-              <div className="col-8">
-                <div className="icheck-primary">
+            <div className='row'>
+              <span className='error-text'>{errors.agreeTerms}</span>
+              <div className='col-8'>
+                <div className='icheck-primary'>
                   <input
-                    type="checkbox"
+                    type='checkbox'
                     checked={agreeTerms}
-                    id="agreeTerms"
-                    name="agreeTerms"
+                    id='agreeTerms'
+                    name='agreeTerms'
                     value={agreeTerms}
                     onChange={handleCheckboxChange}
                   />
-                  <label htmlFor="agreeTerms">
-                    I agree to the <a href="#">terms</a>
+                  <label htmlFor='agreeTerms'>
+                    I agree to the <a href='#'>terms</a>
                   </label>
                 </div>
               </div>
-              <div className="col-4">
-                <button type="submit" className="btn btn-primary btn-block">
+              <div className='col-4'>
+                <button
+                  type='submit'
+                  className='btn btn-primary btn-block'>
                   Register
                 </button>
               </div>
@@ -253,7 +238,9 @@ const Register = () => {
               Sign up using Google+
             </a>
           </div> */}
-          <Link className="text-center" to="/login">
+          <Link
+            className='text-center'
+            to='/login'>
             I already have registered
           </Link>
         </div>
