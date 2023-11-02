cecc3534304cf54914a8e7d3f1036f2c7723db3e
Prince Agrawal <prince.agrawal@dotsquares.com>
2023-11-02 16:55:42 +0530

    patient profile controller code refactoring

--- server/controllers/patient/profile.js
+++ server/controllers/patient/profile.js
@@ -1,47 +1,63 @@
-const bcrypt = require('bcryptjs');
-var jwt = require('jsonwebtoken');
 const PatientProfile = require('../../models/patientProfile');
-const mongoose = require('mongoose');
 
-//Patient Profile create
+// Patient Profile create
 const createPatientProfileController = async (req, res) => {
   try {
-    const profileData = await PatientProfile.findOneAndUpdate(
-      { userId: req.body.userId },
-      req.body,
-      {
-        upsert: true,
-      }
-    );
-    console.log('profileData', profileData);
-    return res.send({
+    const { userId } = req.body;
+
+    // Check if the user's profile already exists
+    let profileData = await PatientProfile.findOne({ userId });
+
+    if (!profileData) {
+      // If the profile doesn't exist, create a new one
+      profileData = new PatientProfile(req.body);
+    } else {
+      // If the profile exists, update it with the new data
+      profileData.set(req.body);
+    }
+
+    await profileData.save();
+
+    return res.status(201).json({
       success: true,
       message: 'Profile Updated Successfully!',
     });
   } catch (error) {
-    //console.log(error);
-    return res.status(200).send({
+    console.error('Error creating/updating patient profile:', error);
+    return res.status(500).json({
       success: false,
-      message: `Register ${error.message}`,
+      message: 'Failed to create/update patient profile.',
+      error: error.message,
     });
   }
 };
 
-//Patient Profile get
+// Patient Profile get
 const getPatientProfileController = async (req, res) => {
   try {
-    const profileData = await PatientProfile.findOne({ userId: req.body.userId });
-    console.log('profileData', profileData);
-    return res.send({
+    const { userId } = req.body;
+
+    const profileData = await PatientProfile.findOne({ userId });
+
+    if (!profileData) {
+      return res.status(404).json({
+        success: false,
+        message: 'Patient profile not found.',
+      });
+    }
+
+    return res.status(200).json({
       success: true,
       data: profileData,
     });
   } catch (error) {
-    //console.log(error);
-    return res.status(200).send({
+    console.error('Error getting patient profile:', error);
+    return res.status(500).json({
       success: false,
-      message: `Register ${error.message}`,
+      message: 'Failed to get patient profile.',
+      error: error.message,
     });
   }
 };
+
 module.exports = { createPatientProfileController, getPatientProfileController };
