eaf3bae1f233ddd3798b389ca99988948d7c7b60
Deepak sharma <depak2084@gmail.com>
2023-12-22 19:36:19 +0530

    constant file

--- client/src/config/config.local.js
+++ client/src/config/config.local.js
@@ -1,4 +1,6 @@
 module.exports = {
   //SERVER_BASE_URL: "http://192.168.6.79:3111",
-  SERVER_BASE_URL: "http://localhost:3111",
+  SERVER_BASE_URL: 'http://localhost:3111',
+  STRIPE_TEST_KEY:
+    'pk_test_51EMqvWCQpJWQbtl10VO5FITrm88QyBtlMEvZ8tyWENUujEKk6extINVGBaQRhXbtz1lnbFwYIx1ADuCilI8lKg8n00p8CyMOrd',
 };
