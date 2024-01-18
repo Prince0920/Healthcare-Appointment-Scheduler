82bb820eb0cc3bab0eeb4daafb6502f61ed3ab63
Deepak sharma <depak2084@gmail.com>
2024-01-18 22:50:03 +0530

    stripe dynamic set

--- server/controllers/paymentCtrl/stripePaymentCtrl.js
+++ server/controllers/paymentCtrl/stripePaymentCtrl.js
@@ -6,37 +6,42 @@ const stripePaymentHelper = require('../../utils/stripePayHelper');
 const patentPayBystripe = async (req, res) => {
   const recordId = req.body.recordId;
   console.log('Record id ', recordId);
-
   try {
-    const stripe = require('stripe')(nodeConstant.STRIPE_TEST_SECRET);
-    const session = await stripe.checkout.sessions.create({
-      success_url: `${nodeConstant.BASE_URL}/patient/booking/payment-success`,
-      cancel_url: `${nodeConstant.BASE_URL}/patient/booking/payment-cancel`,
-      payment_method_types: ['card'],
-      mode: 'payment',
-      line_items: [
-        {
-          price: 'price_1OPJ4TCQpJWQbtl1D26mPUL2', // Replace with your actual price ID
-          quantity: 1,
-        },
-      ],
-    });
+    // const stripe = require('stripe')(nodeConstant.STRIPE_TEST_SECRET);
+    // const session = await stripe.checkout.sessions.create({
+    //   success_url: `${nodeConstant.BASE_URL}/patient/booking/payment-success`,
+    //   cancel_url: `${nodeConstant.BASE_URL}/patient/booking/payment-cancel`,
+    //   payment_method_types: ['card'],
+    //   mode: 'payment',
+    //   line_items: [
+    //     {
+    //       price: 'price_1OPJ4TCQpJWQbtl1D26mPUL2', // Replace with your actual price ID
+    //       quantity: 1,
+    //     },
+    //   ],
+    // });
 
     // window.location = session.url;
     //res.redirect(303, session.url);
     //res.json({ id: session.id });
-    const updateBookingPayStatus =
-      await doctorAppointmentModel.findByIdAndUpdate(recordId, {
-        $set: { paymentStatus: 'completed' },
-      });
 
-    console.log('Payment initiated successfully');
-    console.error(session.url);
+    const sessionRes = await stripePaymentHelper.StripePay('appointBooking');
+    //console.log('sessionRes', sessionRes);
+
+    //return false;
+
+    // const updateBookingPayStatus =
+    //   await doctorAppointmentModel.findByIdAndUpdate(recordId, {
+    //     $set: { paymentStatus: 'completed' },
+    //   });
+
+    //console.log('Payment initiated successfully');
+    // console.error(sessionRes.url);
     res.status(201).send({
       success: true,
       message: 'Payment initiation successful',
-      url: session.url,
-      sessionId: session.id,
+      url: sessionRes.url,
+      sessionId: sessionRes.id,
     });
   } catch (error) {
     console.log(error);
--- server/utils/stripePayHelper.js
+++ server/utils/stripePayHelper.js
@@ -1,22 +1,39 @@
 const nodeConstant = require('../config/nodeConstant');
 
-const StripePayHelper = async () => {
+const StripePay = async (paymenFor) => {
+  console.log('paymenFor', paymenFor);
+
   const stripe = require('stripe')(nodeConstant.STRIPE_TEST_SECRET);
+  let session;
 
-  const session = await stripe.checkout.sessions.create({
-    success_url: `${nodeConstant.BASE_URL}/patient/booking/payment-success`,
-    cancel_url: `${nodeConstant.BASE_URL}/patient/booking/payment-cancel`,
-    payment_method_types: ['card'],
-    mode: 'payment',
-    line_items: [
-      {
-        price: 'price_1OPJ4TCQpJWQbtl1D26mPUL2', // Replace with your actual price ID
-        quantity: 1,
-      },
-    ],
-  });
+  try {
+    if (paymenFor === 'appointBooking') {
+      //console.log('cameee', paymenFor);
 
-  return session;
+      session = await stripe.checkout.sessions.create({
+        success_url: `${nodeConstant.BASE_URL}/patient/booking/payment-success`,
+        cancel_url: `${nodeConstant.BASE_URL}/patient/booking/payment-cancel`,
+        payment_method_types: ['card'],
+        mode: 'payment',
+        line_items: [
+          {
+            price: 'price_1OPJ4TCQpJWQbtl1D26mPUL2', // Replace with your actual price ID
+            quantity: 1,
+          },
+        ],
+      });
+    }
+    // console.log('Stripe Response', session);
+    // return false;
+    return session;
+  } catch (error) {
+    console.log('error in stripe payment', error);
+    // Handle the error appropriately, you might want to throw it or log it.
+    // res.status(500).send({
+    //   success: false,
+    //   message: `Issue in stripe payment. Error: ${error.message}`,
+    // });
+  }
 };
 
-module.exports = { StripePayHelper };
+module.exports = { StripePay };
